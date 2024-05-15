9a32aabfb766a516620618cdae1270b8565e67d8
anezm12 <101163640+anezm12@users.noreply.github.com>
2024-05-14 19:03:19 -0500

    Add files via upload

---
+++ databricks/includes/configuration.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/f1de/raw/'
+processed_folder_path = '/mnt/f1de/processed/'
+presentation_folder_path = '/mnt/f1de/presentation/'
+demo_folder_path = '/mnt/f1de/demo/'
+raw_incremental_load_folder_path = '/mnt/f1de/raw-incremental-load/'
